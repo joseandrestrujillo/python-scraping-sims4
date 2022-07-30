ea1fdb0aec077ac0b850d68b5437176c9c5da24e
joseandrestrujillo <trujillohenaresj@gmail.com>
2022-07-31 01:34:47 +0200

    Set hasStock and price from instant gaming page.

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 import requests
+from bs4 import BeautifulSoup
 
-url = 'https://www.instant-gaming.com/es/12141-comprar-origin-los-sims-4-anos-high-school-pc-mac-juego-origin/'
+url = 'https://www.instant-gaming.com/es/9755-comprar-monster-hunter-rise-sunbreak-pc-juego-steam/'
 
-r = requests.get(url)
+headers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"}
 
-print(r.content)
+r = requests.get(url=url, headers=headers)
 
+soup = BeautifulSoup(r.content, 'html5lib')
+
+hasStock = len(soup.find_all('div', attrs= {'class':'nostock'})) == 0 
+
+if hasStock:
+    amountInfo = soup.find_all('div', attrs= {'class':'panel item wide'})[0].find_all('div', attrs= {'class':'amount'})[0]
+    price = amountInfo.find_all('div', attrs= {'class':'total'})[0].text
+    price = float(price.replace("â‚¬", ""))
